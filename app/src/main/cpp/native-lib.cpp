16e6bc7589a364b88feeb4be16017dccff553aa6
lynx <2104934038@qq.com>
2020-03-16 12:36:16 +0800

    test: 使用Eigen,无视报错

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,23 @@
+#include <Eigen/Core>
+#include <Eigen/SVD>
+#include <Eigen/IterativeLinearSolvers>
+#include <jni.h>
+#include <string>
+
+using namespace std;
+using namespace Eigen;
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_niswgsp_11_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+
+    string hello = "Hello from C++";
+    char msg_test[128];
+    typedef Matrix<int, Dynamic, Dynamic> MyMatrix;
+    MyMatrix test_mat = MyMatrix::Zero(2, 2);
+    test_mat(0, 0) = 1;
+    test_mat(0, 1) = 2;
+    sprintf(msg_test, "[%d %d][%d %d]", test_mat(0, 0), test_mat(0, 1), test_mat(1, 0), test_mat(1, 1));
+    return env->NewStringUTF(msg_test);
+}
