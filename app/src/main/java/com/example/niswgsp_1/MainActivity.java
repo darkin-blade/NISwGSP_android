505419aeb614ab81b41d06d9cc1b801fd3b0f1c1
lynx <2104934038@qq.com>
2020-03-23 14:40:10 +0800

    test: 保存图片

--- app/src/main/java/com/example/niswgsp_1/MainActivity.java
+++ app/src/main/java/com/example/niswgsp_1/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import org.opencv.android.OpenCVLoader;
@@ -12,8 +14,15 @@ import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
     static String appPath;
+    public ImageView imageView;
+    public Bitmap bitmap = null;
+    public Button save_button;
 
     // 初始化opencv java
     static {
@@ -37,6 +46,45 @@ public class MainActivity extends AppCompatActivity {
         appPath = getExternalFilesDir("").getAbsolutePath();
 
         stitch_1();
+        initUI();
+    }
+
+    void saveBmp() {
+        Thread save_bmp = new Thread(new Runnable() {
+            @Override
+            public void run() {
+               if (bitmap != null) {
+                   File file;
+                   for (int i = 0; i < 1000; i ++) {
+                       file = new File(appPath + "/result_" + i + ".jpg");
+                       if (file.exists() == false) {
+                           try {
+                               file.createNewFile();
+                               FileOutputStream stream = new FileOutputStream(file);
+                               bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                               stream.flush();
+                               infoLog("save succeed");
+                               return;
+                           } catch (IOException e) {
+                               e.printStackTrace();
+                           }
+                       }
+                   }
+                   infoLog("save failed");
+               }
+            }
+        });
+        save_bmp.start();
+    }
+
+    void initUI() {
+        save_button = findViewById(R.id.save_button);
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveBmp();
+            }
+        });
     }
 
     void stitch_1() {
@@ -61,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
 
-                final Bitmap bitmap = Bitmap.createBitmap(matBGR.cols(), matBGR.rows(), Bitmap.Config.ARGB_8888);// TODO final
+                bitmap = Bitmap.createBitmap(matBGR.cols(), matBGR.rows(), Bitmap.Config.ARGB_8888);// TODO final
 
                 // BGR转RGB
                 Mat matRGB = new Mat();
@@ -72,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        ImageView imageView = findViewById(R.id.result);
+                        imageView = findViewById(R.id.result);
                         imageView.setImageBitmap(bitmap);
                     }
                 });
